5f724adfedee507c71bc33c8670a0d13f6a09205
Marius Göcke <marius.goecke@gmail.com>
2022-12-04 19:06:12 +0100

    update

--- GRYVAPaperServer/Other/CommonTasks.py
+++ GRYVAPaperServer/Other/CommonTasks.py
@@ -7,20 +7,6 @@ from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjec
 import re
 
 
-def standardized_tasks_update_version_in_docker_examples(file, codeunit_version):
-    folder_of_current_file = os.path.dirname(file)
-    codeunit_folder = GeneralUtilities.resolve_relative_path("..", folder_of_current_file)
-    codeunit_name = os.path.basename(codeunit_folder)
-    codeunit_name_lower = codeunit_name.lower()
-    examples_folder = GeneralUtilities.resolve_relative_path("Other/Examples", codeunit_folder)
-    for example_folder in GeneralUtilities.get_direct_folders_of_folder(examples_folder):
-        docker_compose_file = os.path.join(example_folder, "docker-compose.yml")
-        if os.path.isfile(docker_compose_file):
-            filecontent = GeneralUtilities.read_text_from_file(docker_compose_file)
-            replaced = re.sub(f'image:\\s+[\'"]{codeunit_name_lower}:\\d+\\.\\d+\\.\\d+[\'"]', f"image: '{codeunit_name_lower}:{codeunit_version}'", filecontent)
-            GeneralUtilities.write_text_to_file(docker_compose_file, replaced)
-
-
 def common_tasks():
     file = str(Path(__file__).absolute())
     folder_of_current_file = os.path.dirname(file)
@@ -35,7 +21,7 @@ def common_tasks():
         "../..", os.path.dirname(file)))  # Should always be the same as the project-version
     sc.replace_version_in_dockerfile_file(GeneralUtilities.resolve_relative_path(f"../{codeunitname}/Dockerfile", folder_of_current_file), codeunit_version)
     t.standardized_tasks_do_common_tasks(file, codeunit_version, verbosity, targetenvironmenttype, True, additional_arguments_file, cmd_args)
-    standardized_tasks_update_version_in_docker_examples(file, codeunit_version)
+    t.standardized_tasks_update_version_in_docker_examples(file, codeunit_version)
 
 
 if __name__ == "__main__":
